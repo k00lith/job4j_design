143bc45e98a3cd08d0144719f92470f606b9f413
k00lith <koolith@gmail.com>
2021-05-08 15:02:17 +0300

    1. Что такое итератор. [#473242] поправка условия hasNext

--- src/main/java/ru/job4j/it/BackwardArrayIt.java
+++ src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -14,7 +14,7 @@ public class BackwardArrayIt implements Iterator<Integer> {
 
     @Override
     public boolean hasNext() {
-        return point < data.length;
+        return 0 < data.length;
     }
 
     @Override
